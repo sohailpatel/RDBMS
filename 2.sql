a5658e8410c495e47676ab29b6e2713249ac60b1
sohailpatel <sohailpatelt20@gmail.com>
2016-02-29 19:11:46 +0530

    2nd file added

---
+++ 2.sql
@@ -0,0 +1,20 @@
+ insert into userid(password,type) values("123456789",1);
+ insert into userid(password,type) values("123456789",2);
+ insert into userid(password,type) values("123456789",2);
+ insert into userid(password,type) values("123456789",1);
+
+ insert into userdetalis values(1,"abc","abc@gmail.com");
+  insert into userdetails values(2,"pqr","pqr@gmail.com");
+  insert into userdetails values(3,"xyz","xyz@gmail.com");
+  insert into userdetails values(4,"def","def@gmail.com");
+
+ insert into type values(1,"buyer");
+ insert into type values(2,"seller");
+
+insert into products values(1,"pen",10,"white",1);
+ insert into products values(2,"book",40,"blue",4);
+
+insert into orders values(1,1,2,"2016-02-02");
+  insert into orders values(2,1,1,10,"2016-02-02");
+  insert into orders values(3,2,1,20,"2016-02-03");
+
