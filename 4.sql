1303bd89782e00cfdce05dd381b6bde37f62a3f1
sohailpatel <sohailpatelt20@gmail.com>
2016-03-01 10:23:57 +0530

    Updated 4.sql

--- 4.sql
+++ 4.sql
@@ -1,2 +1,2 @@
-> select name,(select name from userdetails where id in (selleris)) SellerNAme from products group by selleris;
+ select productname.name,(select name from userdetails where id in (products.selleris)) SellerName from products inner join productname on products.id=productname.id group by selleris;
 
