20d4c357e97f922239136d67a7e1f47b9897e8a3
sohailpatel <sohailpatelt20@gmail.com>
2016-03-01 13:12:40 +0530

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -2,20 +2,24 @@
  insert into userid(password,type) values("123456789",2);
  insert into userid(password,type) values("123456789",2);
  insert into userid(password,type) values("123456789",1);
+insert into userid(password,type) values("123456789",1);
 
  insert into userdetalis values(1,"abc","abc@gmail.com");
   insert into userdetails values(2,"pqr","pqr@gmail.com");
   insert into userdetails values(3,"xyz","xyz@gmail.com");
   insert into userdetails values(4,"def","def@gmail.com");
 
- insert into type values(1,"buyer");
- insert into type values(2,"seller");
+ insert into type values(2,"buyer");
+ insert into type values(1,"seller");
+ 
 
 insert into products values(1,10,1,1);
  insert into products values(2,40,4,2);
+insert into products values(3,200,5,1);
 
  insert into productname values(1,"pen");
 	insert into productname values(2,"book");
+ insert into productname values(3,"pen_set");
 
 insert into colorname values(1,"red");
  insert into colorname values(2,"blue");
@@ -26,7 +30,11 @@ insert into colorname values(1,"red");
 
 
 
-insert into orders values(1,1,2,"2016-02-02");
-  insert into orders values(2,1,1,10,"2016-02-02");
-  insert into orders values(3,2,1,20,"2016-02-03");
-
+insert into orders values(1,1,2,20,"2016-02-02",3);
+  insert into orders values(2,1,1,10,"2016-02-02",2);
+  insert into orders values(3,2,1,40,"2016-02-03",2);
+insert into orders values(4,2,2,80,"2016-01-01",3);
+insert into orders values(5,2,1,40,"2016-02-04",2);
+insert into orders values(6,2,3,120,"2016-02-13",2);
+insert into orders values(7,3,1,200,"2016-02-03",5);
+insert into orders values(1,3,1,200,"2016-02-20",5);
