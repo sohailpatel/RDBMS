f45988d799770ce99047afbfc5b9b2e382e0a754
sohailpatel <sohailpatelt20@gmail.com>
2016-03-01 10:23:34 +0530

    Updated 2.sql

--- 2.sql
+++ 2.sql
@@ -11,8 +11,20 @@
  insert into type values(1,"buyer");
  insert into type values(2,"seller");
 
-insert into products values(1,"pen",10,"white",1);
- insert into products values(2,"book",40,"blue",4);
+insert into products values(1,10,1,1);
+ insert into products values(2,40,4,2);
+
+ insert into productname values(1,"pen");
+	insert into productname values(2,"book");
+
+insert into colorname values(1,"red");
+ insert into colorname values(2,"blue");
+ insert into colorname values(3,"white");
+ insert into colorname values(4,"black");
+
+
+
+
 
 insert into orders values(1,1,2,"2016-02-02");
   insert into orders values(2,1,1,10,"2016-02-02");
