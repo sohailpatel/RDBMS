cab29bbae5460b1e9bd9ce0f3d3469cc5617f150
sohailpatel <sohailpatelt20@gmail.com>
2016-03-01 10:24:05 +0530

    Updated 5.sql

--- 5.sql
+++ 5.sql
@@ -1,2 +1,2 @@
- select orders.id,(select userdetails.name from userdetails where userdetails.id in (products.selleris))as SellerName,sum(orders.quantity) from orders,products where orders.productid=products.id group by products.selleris;
+ select orders.id,(select userdetails.name from userdetails where userdetails.id in (products.selleris))as SellerName,sum(orders.quantity) as Total_Orders from orders,products where orders.productid=products.id group by products.selleris;
 
